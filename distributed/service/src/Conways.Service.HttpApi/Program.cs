7f35378f4ed974d2430e6e3dc716134efcc12706
adrielairaldo <adrielairaldo@hotmail.com>
2025-12-28 23:03:49 +0100

    - Add DomainLayerServices to the services middleware - Add regions to separete the regions of Program.cs

--- distributed/service/src/Conways.Service.HttpApi/Program.cs
+++ distributed/service/src/Conways.Service.HttpApi/Program.cs
@@ -1,9 +1,17 @@
+using Conways.Service.Domain;
 var builder = WebApplication.CreateBuilder(args);
 
+#region // Add services to the container
+
+builder.Services.AddDomainLayerServices();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+#endregion // Add services to the container
+
 var app = builder.Build();
+
+#region // Add middlewares to the pipeline
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -13,5 +21,6 @@ if (app.Environment.IsDevelopment())
 }
 
 
+#endregion // Add middlewares to the pipeline
 
 app.Run();
