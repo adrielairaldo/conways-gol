f87c17cf954675a67e100f3cc9d89f14e522a378
adrielairaldo <adrielairaldo@hotmail.com>
2025-12-29 16:25:03 +0100

    Rename SimpleBlinker method to SimpleVerticalBlinker and add SimpleHorizontalBlinker method for grid generation

--- distributed/service/tests/Conways.Service.Domain.Tests/TestData/BasicGridGenerator.cs
+++ distributed/service/tests/Conways.Service.Domain.Tests/TestData/BasicGridGenerator.cs
@@ -4,11 +4,18 @@ namespace Conways.Service.Domain.Tests.TestData;
 
 internal static class BasicGridGenerator
 {
-    public static Grid SimpleBlinker() => new(new List<List<CellState>>
+    public static Grid SimpleVerticalBlinker() => new(new List<List<CellState>>
     {
         new() { CellState.Dead, CellState.Alive, CellState.Dead },
         new() { CellState.Dead, CellState.Alive, CellState.Dead },
         new() { CellState.Dead, CellState.Alive, CellState.Dead }
+    });
+
+    public static Grid SimpleHorizontalBlinker() => new(new List<List<CellState>>
+    {
+        new() { CellState.Dead, CellState.Dead, CellState.Dead },
+        new() { CellState.Alive, CellState.Alive, CellState.Alive },
+        new() { CellState.Dead, CellState.Dead, CellState.Dead }
     });
 
     public static Grid StillLifeBlock() => new(new List<List<CellState>>
