8f14667db9de3a1ca80f7c2442caa214f7539c65
adrielairaldo <adrielairaldo@hotmail.com>
2025-12-29 14:27:25 +0100

    Add Invariants to Grid entity constructor: - EnsureGridIsNotEmpty - EnsureAllRowsHaveSameLength

--- distributed/service/src/Conways.Service.Domain/Boards/Grid.cs
+++ distributed/service/src/Conways.Service.Domain/Boards/Grid.cs
@@ -9,9 +9,35 @@ public sealed class Grid
 
     public Grid(IReadOnlyList<IReadOnlyList<CellState>> cells)
     {
+        EnsureGridIsNotEmpty(cells);
+        EnsureAllRowsHaveSameLength(cells);
+
         Cells = cells;
     }
 
-    public CellState GetCell(int row, int column)
-        => Cells[row][column];
+    public CellState GetCell(int row, int column) => Cells[row][column];
+
+    private static void EnsureGridIsNotEmpty(IReadOnlyList<IReadOnlyList<CellState>> cells)
+    {
+        if (cells is null || cells.Count == 0)
+        {
+            throw new ArgumentException("Grid must contain at least one row.", nameof(cells));
+        }
+    }
+
+    private static void EnsureAllRowsHaveSameLength(IReadOnlyList<IReadOnlyList<CellState>> cellsMatrix)
+    {
+        var expectedColumnCount = cellsMatrix[0].Count;
+
+        var hasInvalidRow = cellsMatrix.Any(row => row.Count != expectedColumnCount);
+
+        if (hasInvalidRow)
+        {
+            throw new ArgumentException
+            (
+                "All rows in the grid must have the same number of columns.",
+                nameof(cellsMatrix)
+            );
+        }
+    }
 }
