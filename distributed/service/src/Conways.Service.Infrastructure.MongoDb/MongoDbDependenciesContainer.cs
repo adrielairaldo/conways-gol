735ce8cf0c38679c7b54ad88b3b56d8040445a0f
adrielairaldo <adrielairaldo@hotmail.com>
2025-12-29 10:54:03 +0100

    Add MongoDB configuration and service registration

---
+++ distributed/service/src/Conways.Service.Infrastructure.MongoDb/MongoDbDependenciesContainer.cs
@@ -0,0 +1,43 @@
+using Conways.Service.Domain.Repositories;
+using Conways.Service.Infrastructure.MongoDb.Repositories;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
+
+namespace Conways.Service.Infrastructure.MongoDb;
+
+public static class MongoDbDependenciesContainer
+{
+    /// <summary>
+    /// Configures MongoDB-related services and health checks for an ASP.NET Core application.
+    /// </summary>
+    /// <param name="services">The IServiceCollection to configure.</param>
+    /// <returns>The IServiceCollection with MongoDB-related services configured.</returns>
+    public static IServiceCollection AddInfrastructureMongoDbServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        // Get MongoDB options from configuration
+        var options = configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>()!;
+
+        BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+        //BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+
+        // Add MongoDB client
+        services.AddSingleton<IMongoClient>(_ => new MongoClient(options.ConnectionString));
+
+        // Add MongoDB database
+        services.AddSingleton<IMongoDatabase>(sp =>
+        {
+            var client = sp.GetRequiredService<IMongoClient>();
+            return client.GetDatabase(options.DatabaseName);
+        });
+
+        // Register MongoBoardRepository as the implementation for IBoardRepository
+        services.AddScoped<IBoardRepository, MongoBoardRepository>();
+
+        return services;
+    }
+}
---
+++ distributed/service/src/Conways.Service.Infrastructure.MongoDb/MongoDbSettings.cs
@@ -0,0 +1,7 @@
+namespace Conways.Service.Infrastructure.MongoDb;
+
+public sealed class MongoDbSettings
+{
+    public string ConnectionString { get; init; } = null!;
+    public string DatabaseName { get; init; } = null!;
+}
