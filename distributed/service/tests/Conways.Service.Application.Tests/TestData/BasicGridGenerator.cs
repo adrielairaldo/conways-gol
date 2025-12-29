e26d89bc28cfcf0474189efcd4f600e7dd5f7c83
adrielairaldo <adrielairaldo@hotmail.com>
2025-12-29 19:25:07 +0100

    Replicate same BasicGridGenerator from Domain.Tests (let's see in the future how to solve this)

---
+++ distributed/service/tests/Conways.Service.Application.Tests/TestData/BasicGridGenerator.cs
@@ -0,0 +1,32 @@
+using Conways.Service.Domain.Boards;
+
+namespace Conways.Service.Application.Tests.TestData;
+
+internal static class BasicGridGenerator
+{
+    public static Grid SimpleVerticalBlinker() => new
+    (
+        [
+            [CellState.Dead, CellState.Alive, CellState.Dead],
+            [CellState.Dead, CellState.Alive, CellState.Dead],
+            [CellState.Dead, CellState.Alive, CellState.Dead]
+        ]
+    );
+
+    public static Grid SimpleHorizontalBlinker() => new
+    (
+        [
+            [CellState.Dead, CellState.Dead, CellState.Dead],
+            [CellState.Alive, CellState.Alive, CellState.Alive],
+            [CellState.Dead, CellState.Dead, CellState.Dead]
+        ]
+    );
+
+    public static Grid StillLifeBlock() => new
+    (
+        [
+            [CellState.Alive, CellState.Alive],
+            [CellState.Alive, CellState.Alive]
+        ]
+    );
+}
