7d787342f87705ff79ba268ae217ea6a3ea4ede7
adrielairaldo <adrielairaldo@hotmail.com>
2025-12-30 09:13:43 +0100

    Move BasicGridGenerator to Domain project to avoid duplication of the class

---
+++ distributed/service/src/Conways.Service.Domain/TestData/BasicGridGenerator.cs
@@ -0,0 +1,50 @@
+using Conways.Service.Domain.Boards;
+
+namespace Conways.Service.Domain.TestData;
+
+public static class BasicGridGenerator
+{
+    /* This class contains a set of grids used to avoid repetition and increase clarity
+     * in test cases. Since it is used in different projects, to avoid repetition we
+     * leave the class in the Domain project, making a pragmatic decision. */
+
+    public static Grid SimpleVerticalBlinker() => new
+    (
+        [
+            [CellState.Dead, CellState.Alive, CellState.Dead],
+            [CellState.Dead, CellState.Alive, CellState.Dead],
+            [CellState.Dead, CellState.Alive, CellState.Dead]
+        ]
+    );
+
+    public static Grid SimpleHorizontalBlinker() => new
+    (
+        [
+            [CellState.Dead, CellState.Dead, CellState.Dead],
+            [CellState.Alive, CellState.Alive, CellState.Alive],
+            [CellState.Dead, CellState.Dead, CellState.Dead]
+        ]
+    );
+
+    public static Grid StillLifeBlock() => new
+    (
+        [
+            [CellState.Alive, CellState.Alive],
+            [CellState.Alive, CellState.Alive]
+        ]
+    );
+
+    public static Grid SingleAliveCellGrid() => new
+    (
+        [
+            [CellState.Alive]
+        ]
+    );
+
+    public static Grid SingleDeadCellGrid() => new
+    (
+        [
+            [CellState.Dead]
+        ]
+    );
+}
