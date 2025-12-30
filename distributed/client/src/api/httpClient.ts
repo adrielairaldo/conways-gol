81f6e8fb79bfb60151d791f0e500f935721a0342
adrielairaldo <adrielairaldo@hotmail.com>
2025-12-30 19:27:34 +0100

    Wrap axios logic in httpClient, and expose httpClient with verbs

---
+++ distributed/client/src/api/httpClient.ts
@@ -0,0 +1,21 @@
+import axios, { type AxiosInstance, type AxiosResponse } from "axios";
+
+
+const axiosInstance: AxiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL ?? "http://localhost:5000",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Extract only the data
+const getResponseBody = <T>(response: AxiosResponse<T>) => response.data;
+
+// Declarative HTTP verbs
+export const httpClient = {
+  get: <T>(url: string) => axiosInstance.get<T>(url).then(getResponseBody),
+  post: <T>(url: string, data?: any, config?: any) => axiosInstance.post<T>(url, data, config).then(getResponseBody),
+  put: <T>(url: string, data?: any, config?: any) => axiosInstance.put<T>(url, data, config).then(getResponseBody),
+  patch: <T>(url: string, data?: any, config?: any) => axiosInstance.patch<T>(url, data, config).then(getResponseBody),
+  del: <T>(url: string) => axiosInstance.delete<T>(url).then(getResponseBody),
+};
