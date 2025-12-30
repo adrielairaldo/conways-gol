6e4a33380a1c0bd8502a61e959555028eec903ad
adrielairaldo <adrielairaldo@hotmail.com>
2025-12-30 19:30:08 +0100

    Add advanceBoard http call to boardsApi

--- distributed/client/src/api/boardsApi.ts
+++ distributed/client/src/api/boardsApi.ts
@@ -1,3 +1,4 @@
+import type { AdvanceBoardRequest } from './contracts/AdvanceBoardRequest';
 import type { BoardState } from '../domain/BoardState';
 import type { CreateBoardRequest } from './contracts/CreateBoardRequest';
 import type { CreateBoardResponse } from './contracts/CreateBoardResponse';
@@ -6,4 +7,5 @@ import { httpClient } from './httpClient';
 export const BoardsApi = {
   createBoard: (request: CreateBoardRequest) => httpClient.post<CreateBoardResponse>("/api/Boards", request),
   getBoard: (boardId: string) => httpClient.get<BoardState>(`/api/Boards/${boardId}`),
+  advanceBoard: (boardId: string, request: AdvanceBoardRequest) => httpClient.post<BoardState>(`/api/Boards/${boardId}/advance`, request),
 };
