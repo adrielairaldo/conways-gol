9391620e7c91f3ca58d83074a431dcdd2f07ec55
adrielairaldo <adrielairaldo@hotmail.com>
2026-01-07 22:57:18 +0100

    Remove comments

--- distributed/service/src/Conways.Service.HttpApi/Program.cs
+++ distributed/service/src/Conways.Service.HttpApi/Program.cs
@@ -27,11 +27,8 @@ var app = builder.Build();
 #region // Add middlewares to the pipeline
 
 // Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-//}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseCustomCors(builder.Configuration);
 
