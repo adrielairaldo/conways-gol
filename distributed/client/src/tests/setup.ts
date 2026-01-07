acfc37f57a4a156ac4fb2eee26d0699adba9a78d
adrielairaldo <adrielairaldo@hotmail.com>
2026-01-07 17:51:03 +0100

    Add Vitest and React testing-library to client

---
+++ distributed/client/src/tests/setup.ts
@@ -0,0 +1,12 @@
+import * as matchers from '@testing-library/jest-dom/matchers';
+import { afterEach, expect } from 'vitest';
+import { cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/vitest';
+
+// Extend Vitest's expect with jest-dom matchers
+expect.extend(matchers);
+
+// Cleanup after each test
+afterEach(() => {
+    cleanup();
+});
--- distributed/client/vite.config.ts
+++ distributed/client/vite.config.ts
@@ -1,6 +1,8 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,5 +13,11 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss()
-  ]
+  ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/tests/setup.ts',
+    css: true,
+  }
 })
